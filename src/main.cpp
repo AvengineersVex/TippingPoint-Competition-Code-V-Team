65d1b84420aec5fca9de378db3282f839e240ec7
ryv10 <74744128+ryv10@users.noreply.github.com>
2021-10-18 10:13:36 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,12 @@ using namespace vex;
 // A global instance of competition
 competition Competition;
 
+void pre_auton(void) {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
+
 void autonomous(void) {
 
 }
@@ -29,12 +35,13 @@ void usercontrol(void) {
 }
 
 int main() {
+  // Set up callbacks for autonomous and driver control periods.
   Competition.autonomous(autonomous);
   Competition.drivercontrol(usercontrol);
 
-  // Initializing Robot Configuration. DO NOT REMOVE!
-  vexcodeInit();
-
+  // Run the pre-autonomous function.
+  pre_auton();
+  
   // Prevent main from exiting with an infinite loop.
   while (true) {
     wait(100, msec);
